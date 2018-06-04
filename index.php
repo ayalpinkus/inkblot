27d20a85ba167ad90faebab1a627b45be29a45e0
Ayal Pinkus <apinkus@xs4all.nl>
2018-06-04 18:17:34 +0200

    Some more fixes.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+
+
+
+
 <?php
 /**
  * The main template file.
@@ -14,7 +18,8 @@ get_header(); ?>
 	
 	<?php
 
-/*
+/*******************************************************
+
 The following modifications require the viewr to pass ?lastpost=<post_name> in the url, and all posts from the post with that post name up to the first post will be shown.
 
 If a post with post_name lastpost does not exist, no posts are shown.
@@ -22,7 +27,9 @@ This is designed to be an archive for webcomics, designed so that only people wh
 
 If you change $renderall = false; to $renderall = true; in the line below, the code will revert to the default and show everything.
 
-*/
+********************************************************/
+
+
 
 $renderall = false;
 
@@ -37,40 +44,46 @@ if ( $lastpost != "") {
       'post_type' => 'post',
       'post_status' => 'publish',
       'posts_per_page' => 1
-  ) ) ) $found_post = $posts_search[0];
-}
-
-if ( is_null( $found_post ) && ! $renderall ){
-  echo "<p style='font-size:36pt;'>Sorry, the post you are looking for was not found...</p>";
-}
-
-$canshow=$renderall;
-
-
-		if (have_posts()) :
-			while (have_posts()) : the_post();
-
-
-$post = get_post( $post );
-$post_name = isset( $post->post_name ) ? $post->post_name : '';
-if ($post_name == $lastpost) {
-  if (! is_null( $found_post )) {
-    $canshow = true;
+  ) ) ) {
+    $found_post = $posts_search[0];
   }
 }
 
-if ($canshow) {
+if ( is_null( $found_post ) && ! $renderall ){
+  if ( $lastpost == "") {
+    echo "<h1>Congratulations!</h1>You have come to the right place!<p>This is the archive for the @@@ comic. Please visit @@@Site Here@@@ to subscribe to my newsletter. I will send out a new episode for my webcomic every @@@, and as soon as you receive your first newsletter, you can read the episodes from the start on this website.</p>";
+  }
+  else
+  {
+    echo "<p style='font-size:36pt;'>Sorry, the post you are looking for was not found...</p>";
+  }
+}
+else
+{
+  $canshow=true;
 
-				get_template_part('content', get_post_format());
+  $custom_query = new WP_Query('order=asc'); 
+  if (have_posts()) :
+    while($custom_query->have_posts()) : $custom_query->the_post();
+    if ($canshow) {
+      get_template_part('content', get_post_format());
+    }
+
+    $post = get_post( $post );
+    $post_name = isset( $post->post_name ) ? $post->post_name : '';
+    if ($post_name == $lastpost) {
+      if (! is_null( $found_post )) {
+	$canshow = $renderall;
+      }
+    }
+    endwhile;
+    print inkblot_posts_nav(false, get_theme_mod('paged_navigation', true));
+  else :
+    get_template_part('content', 'none');
+  endif;
 }
 
-			endwhile;
 
-			
-			print inkblot_posts_nav(false, get_theme_mod('paged_navigation', true));
-		else :
-			get_template_part('content', 'none');
-		endif;
 	?>
 	
 </main>
